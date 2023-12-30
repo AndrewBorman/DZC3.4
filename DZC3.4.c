928db0c724e8085bc7ea2e541ff0fc54d6ff76f0
Andrew <abaravy@gmail.com>
2023-12-30 19:57:55 +0300

    first commit

---
+++ DZC3.4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int k, l, m;
+float sr ;
+
+int main()
+{
+    printf("Wwedite tri 4isla\n");
+    scanf("%d %d %d", &k, &l, &m);
+    sr = (k+l+m)/3.0;
+    printf("%.2f\n", sr);
+      
+    return 0;
+}
